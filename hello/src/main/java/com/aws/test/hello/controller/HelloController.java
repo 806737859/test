7fbca5839b8c25c98fbafd630dc0a2893125739e
aiwusheng <806737859@qq.com>
2019-09-17 11:48:47 +0800

    add hello demo

---
+++ hello/src/main/java/com/aws/test/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.aws.test.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/say")
+    public String sayHello(){
+        return "hello!";
+    }
+
+}
