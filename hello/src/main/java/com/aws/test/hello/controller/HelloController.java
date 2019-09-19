58cf84ee6f19f2e8aae5f80317132b2d04f5e867
aiwusheng <806737859@qq.com>
2019-09-19 16:18:03 +0800

    jenkins test six!

--- hello/src/main/java/com/aws/test/hello/controller/HelloController.java
+++ hello/src/main/java/com/aws/test/hello/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/say")
     public String sayHello(){
-        return "no hello, payload success!!! jenkins test five!";
+        return "no hello, payload success!!! jenkins test six!";
     }
 
 }
